1066004b6c732f9ceb9444523090cc750d35c0a8
Guilherme <guilherme.forte17@gmail.com>
2025-05-14 21:03:12 -0300

    feat: improve portfolio landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import Layout from "pages/components/layout";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
